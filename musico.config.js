6837c7b9db237067e12490ef0aafd79bcf3f569d
ferdiansyah <68240457+Feranzu@users.noreply.github.com>
2022-07-21 19:23:30 +0700

    Add files via upload

---
+++ musico.config.js
@@ -0,0 +1,13 @@
+/**
+ * @typedef {Object} Config
+ * @property {string} [botId] => Your's bot's client id
+ * @property {string} [botToken] => The bot's token
+ * @property {string} [ownerId] => The bot's owner's / developer's id
+ * @note you can add other properties to this object by yourself and access them in your code using `client.config.<your-property>`
+ */
+const config = {
+	botId: "997732189120778350",
+	botToken: "OTk3NzMyMTg5MTIwNzc4MzUw.GOblwY.TcBTsnaWx2SKV4V3isY6szD-Vxjqgo3TpgJnrY",
+	ownerId: "399089148960440350",
+};
+module.exports = config;
